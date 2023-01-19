7338e378d80572537577630c5c4be0ec418897ad
lyonga <chrlslyonga@gmail.com>
2023-01-19 17:51:34 -0500

    rest

--- main.tf
+++ main.tf
@@ -16,12 +16,3 @@ resource "aws_s3_bucket" "data-science-storage"{
     }
   
   }
-  resource "aws_s3_bucket_public_access_block" "data-science-storage" {
-    bucket = aws_s3_bucket.data-science-storage.id
-  
-    block_public_acls       = true
-    block_public_policy     = true
-    ignore_public_acls      = true
-    restrict_public_buckets = true
-    depends_on = [aws_s3_bucket.data-science-storage]
-  }
